e6e00c502d1f46405ff557fc8a4409a38bc27545
LaughingCoffinLawl <36084465+LaughingCoffinLawl@users.noreply.github.com>
2024-11-17 21:36:35 +0100

    Changed dungeon creation from manual prefabs, to a random algorithm (graphs)

--- DungeonsPrefab.h
+++ DungeonsPrefab.h
@@ -2,12 +2,7 @@
 #include "Room.h"
 #include "EmptyRoom.h"
 #include "StartRoom.h"
-#include "NormalRoom.h"
 #include "TreasureRoom.h"
-#include "BossRoom.h"
-#include "Dungeon.h"
-
-#include <map>
 
 #ifndef DUNGEONS_PREFAB_H
 #define DUNGEON_PREFAB_H
